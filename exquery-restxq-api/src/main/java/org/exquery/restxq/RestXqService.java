19d3fe6a65a85520600177f76556b5af97b24fee
Adam Retter <adam.retter@googlemail.com>
2012-05-29 21:39:26 +0100

    [feature] basis for the RESTXQ Service

---
+++ exquery-restxq-api/src/main/java/org/exquery/restxq/RestXqService.java
@@ -0,0 +1,67 @@
+/*
+Copyright (c) 2012, Adam Retter
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of Adam Retter Consulting nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL Adam Retter BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.exquery.restxq;
+
+import org.exquery.http.HttpRequest;
+import org.exquery.http.HttpResponse;
+
+/**
+ * Representation of a RESTful XQuery Service
+ * 
+ * Really just an intermediary for invoking a ResourceFunction
+ * on a request and returning the response
+ *
+ * @author Adam Retter <adam.retter@googlemail.com>
+ */
+public interface RestXqService extends Comparable<RestXqService> {    
+    
+    /**
+     * Get the XQuery ResourceFunction which
+     * backs this Service
+     * 
+     * @return The ResourceFunction for execution by this service
+     */
+    public ResourceFunction getResourceFunction();
+    
+    /**
+     * Determines if this service can Service the request
+     * 
+     * @param httpRequest The http request. This method must not consume the request body.
+     * 
+     * @return true if this Service can service the incoming request
+     */
+    public boolean canService(final HttpRequest httpRequest);
+    
+    /**
+     * Service the incoming HttpRequest with the RESTXQ Service
+     * and write the response to the HttpResponse
+     * 
+     * @param httpRequest The request to service
+     * @param httpResponse The response to write the service result to
+     */
+    public void service(final HttpRequest httpRequest, final HttpResponse httpResponse);
+}
