0d5f6f61b8a414de943a6435330dba59b033d5ae
Adam Retter <adam.retter@googlemail.com>
2012-09-17 19:10:07 +0100

    [bugfix] avoid loss of precision

--- exquery-restxq/src/main/java/org/exquery/restxq/impl/AbstractRestXqService.java
+++ exquery-restxq/src/main/java/org/exquery/restxq/impl/AbstractRestXqService.java
@@ -319,6 +319,13 @@ public abstract class AbstractRestXqService implements RestXqService {
             return 1;
         }
         
-        return getResourceFunction().getPathAnnotation().getPathSpecificityMetric() - ((RestXqService)other).getResourceFunction().getPathAnnotation().getPathSpecificityMetric();
+        long result = getResourceFunction().getPathAnnotation().getPathSpecificityMetric() - ((RestXqService)other).getResourceFunction().getPathAnnotation().getPathSpecificityMetric();
+        if(result > 0) {
+            return 1;
+        } else if(result < 0) {
+            return -1;
+        } else {
+            return 0;
+        }
     }
 }
